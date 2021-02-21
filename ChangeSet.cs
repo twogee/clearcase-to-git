008d6fdac61d7b866316053b209a8a968bd26875
twogee <g.grigelionis@gmail.com>
2021-02-21 15:12:04 +0100

    Use default values

--- ChangeSet.cs
+++ ChangeSet.cs
@@ -129,12 +129,7 @@ namespace GitImporter
             Labels = new List<string>();
         }
 
-        public NamedVersion Add(ElementVersion version)
-        {
-            return Add(version, null, true);
-        }
-
-        public NamedVersion Add(ElementVersion version, string name, bool inRawChangeSet)
+        public NamedVersion Add(ElementVersion version, string name = null, bool inRawChangeSet = true)
         {
             NamedVersion result;
             NamedVersion existing = Versions.Find(v => v.Version.Element == version.Element);
